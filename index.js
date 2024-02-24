c206202aa37c6d15d8d44bbe00a798b4701b7002
LaAlicia <160994395+LaAlicia@users.noreply.github.com>
2024-02-24 15:31:53 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const red = 163;
+const green = 129;
+const blue = 124;
+
+
+// About Me Redirection function
+function redirectToPage() {
+    // Use window.location.href to navigate to the desired page
+    window.location.href = 'aboutMe.html';
+}
